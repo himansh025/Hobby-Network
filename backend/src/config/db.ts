612ccb4c2c1b65bc862435ff69256dba32fa3eb6
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-10-16 21:13:51 +0530

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/cybernauts');
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+return;
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
